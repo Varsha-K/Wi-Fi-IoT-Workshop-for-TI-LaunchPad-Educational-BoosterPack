a0dd27f1c17bd35e52341847ecec1efbae168460
Varsha-K <varsha.k209@gmail.com>
2017-01-20 12:02:51 -0500

    Rename TembooAccount.h to Lab 3 TembooAccount.h

--- TembooAccount.h
+++
@@ -1,6 +0,0 @@
-#define TEMBOO_ACCOUNT "varsha"  // Your Temboo account name 
-#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app name
-#define TEMBOO_APP_KEY "NsJp8rap1Z7gKLEp6sPwmtgga2Ti9t6e"  // Your Temboo app key
-
-#define WIFI_SSID "TEXINS3"
-#define WPA_PASSWORD "launchpad"
